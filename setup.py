37f7f2bb54d6bd449cfc405ed6a9e0095aff44cd
peng <sp900409@gmail.com>
2017-10-23 23:27:25 -0700

    django restapi demo code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='DjangoRest',
+    version='',
+    packages=['snippets', 'snippets.migrations', 'DjangoRest'],
+    url='',
+    license='',
+    author='peng',
+    author_email='',
+    description=''
+)
