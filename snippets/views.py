038ea3fd7c0ae7f449bdbd8bc256f4d634df307f
peng <sp900409@gmail.com>
2017-10-24 00:32:16 -0700

    finish api-auth

--- snippets/views.py
+++ snippets/views.py
@@ -20,17 +20,35 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 
 
+from django.contrib.auth.models import User
+from serializers import UserSerializer
+
+from rest_framework import permissions
+
+from snippets.permissions import IsOwnerOrReadOnly
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 
 
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # class SnippetList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
