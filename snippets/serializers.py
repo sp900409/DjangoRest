d01f87f36a22d6f2824cc6c232b4dc26d7b8249c
peng <sp900409@gmail.com>
2017-10-24 22:29:18 -0700

    finish relationships and hyperlinked apis tutorial 5

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
@@ -16,8 +17,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserSerializerfields = ('id', 'username', 'snippets')
 
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
+    class Meta:
+        model = Snippet
+        fields = ('url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'id', 'username', 'snippets')
 
 
 
